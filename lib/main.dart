cea85d9627d5c98a715ccf4870f8e6b7b239841b
jayachandranmd <jayachandranmd.contact@gmail.com>
2022-09-06 23:03:36 +0530

    Added primaryswatch theme for app

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:text_recognition/screens/hidden_drawer.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        home: const HiddenDrawer());
+  }
+}
